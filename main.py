4f78637bdefe2a1ff7418c93edb2b512d86a6b1b
Konrad Rodak <rodakconrad.rc@gmail.com>
2025-04-27 22:24:25 +0200

    Generacja płaszczyzn zgodnie z instrukcją

--- main.py
+++ main.py
@@ -1,18 +1,53 @@
 from scipy.stats import norm
 from csv import writer
+import numpy as np
 
+def generacja_pozioma(liczba_punktow):
+    dystrybucja_x = norm(loc=0, scale=20)
+    dystrybucja_y = norm(loc=0, scale=200)
+    dystrybucja_z = norm(loc=0.2, scale=0.05)
 
-distribution_x = norm(loc=0, scale=100)
-distribution_y = norm(loc=0, scale=100)
-distribution_z = norm(loc=0.2, scale=0.05)
+    x = dystrybucja_x.rvs(size=liczba_punktow)
+    y = dystrybucja_y.rvs(size=liczba_punktow)
+    z = dystrybucja_z.rvs(size=liczba_punktow)
 
-num_points = 1000
-x = distribution_x.rvs(size=num_points)
-y = distribution_y.rvs(size=num_points)
-z = distribution_z.rvs(size=num_points)
+    punkty = zip(x, y, z)
+    with open('Lab1DataPozioma.xyz', 'w', encoding='utf-8', newline='\n') as csvfile:
+        csvwriter = writer(csvfile)
+        for p in punkty:
+            csvwriter.writerow(p)
 
-points = zip(x, y, z)
-with open('Lab1DataPozioma.xyz', 'w', encoding='utf-8', newline='\n') as csvfile:
-    csvwriter = writer(csvfile)
-    for p in points:
-        csvwriter.writerow(p)
+def generacja_pionowa(liczba_punktow):
+    dystrybucja_x = norm(loc=0, scale=20)
+    dystrybucja_y = norm(loc=0.2, scale=0.05)
+    dystrybucja_z = norm(loc=0, scale=200)
+
+    x = dystrybucja_x.rvs(size=liczba_punktow)
+    y = dystrybucja_y.rvs(size=liczba_punktow)
+    z = dystrybucja_z.rvs(size=liczba_punktow)
+
+    punkty = zip(x, y, z)
+    with open('Lab1DataPionowa.xyz', 'w', encoding='utf-8', newline='\n') as csvfile:
+        csvwriter = writer(csvfile)
+        for p in punkty:
+            csvwriter.writerow(p)
+
+def generacja_cylidryczna(liczba_punktow):
+    kat = np.pi
+    dystrybucja_x = norm(loc=0, scale=kat)
+    dystrybucja_y = norm(loc=0.5, scale=20)
+    dystrybucja_z = norm(loc=0, scale=kat)
+
+    x = dystrybucja_x.rvs(size=liczba_punktow)
+    y = dystrybucja_y.rvs(size=liczba_punktow)
+    z = dystrybucja_z.rvs(size=liczba_punktow)
+
+    punkty = zip(x, y, z)
+    with open('Lab1DataCylider.xyz', 'w', encoding='utf-8', newline='\n') as csvfile:
+        csvwriter = writer(csvfile)
+        for p in punkty:
+            csvwriter.writerow(p)
+
+generacja_pozioma(2000)
+generacja_pionowa(2000)
+generacja_cylidryczna(2000)
