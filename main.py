c201fb4ff4573ce38d229345fcaf9306da846622
Konrad Rodak <rodakconrad.rc@gmail.com>
2025-05-03 20:29:02 +0200

    Wczytywanie pliku i punktów z niego

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ from sklearn.cluster import KMeans
 from scipy.spatial.distance import cdist
 
 def generacja_pozioma(liczba_punktow):
-    dystrybucja_x = norm(loc=0, scale=20)
+    dystrybucja_x = norm(loc=0, scale=100)
     dystrybucja_y = norm(loc=0, scale=200)
     dystrybucja_z = norm(loc=0.2, scale=0.05)
 
@@ -21,7 +21,7 @@ def generacja_pozioma(liczba_punktow):
             csvwriter.writerow(p)
 
 def generacja_pionowa(liczba_punktow):
-    dystrybucja_x = norm(loc=0, scale=20)
+    dystrybucja_x = norm(loc=0, scale=100)
     dystrybucja_y = norm(loc=0.2, scale=0.05)
     dystrybucja_z = norm(loc=0, scale=200)
 
@@ -36,14 +36,12 @@ def generacja_pionowa(liczba_punktow):
             csvwriter.writerow(p)
 
 def generacja_cylidryczna(liczba_punktow):
-    kat = np.pi
-    dystrybucja_x = norm(loc=0, scale=kat)
-    dystrybucja_y = norm(loc=0.5, scale=20)
-    dystrybucja_z = norm(loc=0, scale=kat)
+    kat = np.linspace(0, 2 * np.pi, liczba_punktow, endpoint=False)
+    x = 50 * np.cos(kat)
+    dystrybucja_y = norm(loc=0.5, scale=50)
+    z = 50 * np.sin(kat)
 
-    x = dystrybucja_x.rvs(size=liczba_punktow)
     y = dystrybucja_y.rvs(size=liczba_punktow)
-    z = dystrybucja_z.rvs(size=liczba_punktow)
 
     punkty = zip(x, y, z)
     with open('Lab1DataCylider.xyz', 'w', encoding='utf-8', newline='\n') as csvfile:
